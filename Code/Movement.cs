dfd1577321ca2710d0153d4fe11b72094017b050
kosij <justinkoytb@gmail.com>
2026-04-14 14:47:09 +0100

    feat: implement manual gravity and jumping

--- Code/Movement.cs
+++ Code/Movement.cs
@@ -4,6 +4,8 @@ public sealed class Movement : Component
 {
     // These show up in your Inspector so you can tweak them without code
     [Property] public float Speed { get; set; } = 300f;
+    [Property] public float Gravity { get; set; } = 800f;
+    [Property] public float JumpForce { get; set; } = 300f;
 
     // This is the "Physics" part we talked about
     [RequireComponent] public CharacterController Controller { get; set; }
@@ -15,12 +17,29 @@ public sealed class Movement : Component
 
         // make direction relative to camera
         wishDir *= Scene.Camera.WorldRotation;
-        
-        // remove z direction
+
         wishDir.z = 0;
 
-        // tell character controller where to go
-        Controller.Velocity = wishDir * Speed;
+        Vector3 targetVelocity = wishDir * Speed;
+
+        targetVelocity.z = Controller.Velocity.z;
+
+        if ( Controller.IsOnGround )
+        {
+            targetVelocity.z = 0;
+
+        }
+        else
+        {
+            targetVelocity.z -= Gravity * Time.Delta;
+        }
+
+        Controller.Velocity = targetVelocity;
+
+        if ( Controller.IsOnGround && Input.Pressed( "jump" ) )
+        {
+            Controller.Punch( Vector3.Up * JumpForce );
+        }
 
         Controller.Move();
     }
