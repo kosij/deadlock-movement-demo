5a6e6ce85809325690a65707e4e5bf116460cef7
kosij <justinkoytb@gmail.com>
2026-04-14 15:26:09 +0100

    feat: add third-person camera

---
+++ Code/PlayerCamera.cs
@@ -0,0 +1,32 @@
+using Sandbox;
+using System;
+
+public sealed class PlayerCamera : Component
+{
+    [Property] public GameObject Target { get; set; }
+    [Property] public float Distance { get; set; } = 250f;
+    [Property] public float TargetHeight { get; set; } = 64f;
+
+    private Angles EyeAngles;
+
+    protected override void OnUpdate()
+    {
+        if ( Target == null ) return;
+
+        // get mouse input
+        EyeAngles += Input.AnalogLook;
+
+        // stop camera from turning upside down
+        EyeAngles.pitch = EyeAngles.pitch.Clamp( -89f, 89f );
+
+        // apply rotation to scene camera
+        Scene.Camera.WorldRotation = EyeAngles.ToRotation();
+
+        // set the camera offset from the player
+        Vector3 centerPosition = Target.Transform.Position + Vector3.Up * TargetHeight;
+        Vector3 offset = Scene.Camera.WorldRotation.Backward * Distance;
+
+        // apply position to scene camera
+        Scene.Camera.WorldPosition = centerPosition + offset;
+    }
+}
